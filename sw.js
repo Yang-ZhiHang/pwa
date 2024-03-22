1f397f1aa533ae466cbef0bfe16a109630a6f3c6
ZamHaaz Young <2097853195@qq.com>
2024-03-22 15:06:41 +0800

    init project

---
+++ sw.js
@@ -0,0 +1,50 @@
+
+if (navigator.serviceWorker != null) {
+  navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    console.log('Registered events at scope: ', registration.scope);
+  });
+}
+var cacheStorageKey = 'minimal-pwa-1'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "main.css",
+  "favicon.png"
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
