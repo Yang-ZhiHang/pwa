0bbe32bbf1fc2c3afb1f9ea90947ace3d8adcda2
ZamHaaz Young <2097853195@qq.com>
2024-03-22 15:25:45 +0800

    2

--- sw.js
+++ sw.js
@@ -5,3 +5,36 @@ if (navigator.serviceWorker != null) {
     console.log('Registered events at scope: ', registration.scope);
   });
 }
+
+var cacheStorageKey = 'minimal-pwa-1'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "main.css",
+  "favicon.png"
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
